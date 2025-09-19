a29730b87980df48d8921012e725eb645aa7b3fb
候机楼 <382626123@qq.com>
2025-09-19 19:24:36 +0800

    增加随机数

--- src/WdsUtil.php
+++ src/WdsUtil.php
@@ -1,7 +1,27 @@
 <?php
 namespace Wds\Util;
 
-class MyUtil{
+class WdsUtil{
+
+
+    /**
+     * @param $lenght
+     * @return string
+     * @throws \Random\RandomException
+     * 获取随机数
+     */
+    public static function uniqidReal($lenght = 13):string
+    {
+        if (function_exists("random_bytes")) {
+            $bytes = random_bytes(ceil($lenght / 2));
+        } elseif (function_exists("openssl_random_pseudo_bytes")) {
+            $bytes = openssl_random_pseudo_bytes(ceil($lenght / 2));
+        } else {
+            throw new \Exception("no cryptographically secure random function available");
+        }
+        return substr(bin2hex($bytes), 0, $lenght);
+    }
+
 
     /**
      * @param string $total 总金额
